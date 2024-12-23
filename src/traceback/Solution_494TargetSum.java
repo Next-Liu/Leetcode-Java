3d19090f9cf5e352bec9f682aea62269f8360005
Next <2291572118@qq.com>
2024-12-23 22:39:46 +0800

    traceback-3

--- src/traceback/Solution_494TargetSum.java
+++ src/traceback/Solution_494TargetSum.java
@@ -4,15 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution_494TargetSum {
-    public void dfs(int total, int element, int res, int target) {
-        if (total == target) {
-            res+=1;
+    public void dfs(int total, int[] nums, int target, int[] res, int i) {
+        // 如果总和等于目标且已遍历完所有数字
+        if (total == target && i == nums.length) {
+            res[0] += 1; // 使用数组来传递和修改结果
             return;
         }
-        dfs(total+element,);
+
+        // 如果已遍历完所有数字，但总和不等于目标，返回
+        if (i == nums.length) {
+            return;
+        }
+
+        // 递归处理加上当前数字的情况
+        dfs(total + nums[i], nums, target, res, i + 1);
+
+        // 递归处理减去当前数字的情况
+        dfs(total - nums[i], nums, target, res, i + 1);
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+        int[] res = new int[1]; // 使用数组来存储结果，因为整数是不可变的
+        dfs(0, nums, target, res, 0);
+        return res[0];
     }
 
-    public int findTargetSumWays(int[] nums, int target) {
-        List<Integer> path = new ArrayList<>();
+    public static void main(String[] args) {
+        Solution_494TargetSum ss = new Solution_494TargetSum();
+        int[] nums = {1, 1, 1};
+        int target = 3;
+        ss.findTargetSumWays(nums, target);
     }
 }
