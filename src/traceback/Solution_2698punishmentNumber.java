3cda498547a582957fde00692253184d38d3b981
next <2291572118@qq.com>
2024-12-27 01:27:29 +0800

    traceback-6

--- src/traceback/Solution_2698punishmentNumber.java
+++ src/traceback/Solution_2698punishmentNumber.java
@@ -24,9 +24,6 @@ public class Solution_2698punishmentNumber {
 
     public boolean judge(int num, List<String> list) {
         int sum = 0;
-        if (list.size()==1 && num !=1){
-            return false;
-        }
         for (String s : list) {
             sum += Integer.parseInt(s);
         }
@@ -35,7 +32,8 @@ public class Solution_2698punishmentNumber {
 
     public void dfs(int start, String num, List<String> path) {
         if (start == num.length()) {
-            if (judge(Integer.parseInt(num), path)) {
+            int i = Integer.parseInt(num);
+            if (judge((int)Math.sqrt(i), path)) {
                 res = true;
             }
             return;
@@ -51,10 +49,10 @@ public class Solution_2698punishmentNumber {
     public int punishmentNumber(int n) {
         int sum = 0;
         for (int i = 0; i < n; i++) {
-            String num = String.valueOf((i+1) * (i+1));
+            String num = String.valueOf((i + 1) * (i + 1));
             dfs(0, num, new ArrayList<>());
             if (res) {
-                sum += (i+1) * (i+1);
+                sum += (i + 1) * (i + 1);
             }
             res = false;
         }
@@ -63,6 +61,7 @@ public class Solution_2698punishmentNumber {
 
     public static void main(String[] args) {
         Solution_2698punishmentNumber ss = new Solution_2698punishmentNumber();
-        int i = ss.punishmentNumber(10);
+        int i = ss.punishmentNumber(37);
+        System.out.println(i);
     }
 }
